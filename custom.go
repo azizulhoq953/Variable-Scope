234fc67b6aba2e6f3ddb47693d1bdb0fee1e965e
azizul_hoq <azizulhoq4305@gamil.com>
2021-02-16 20:24:03 +0600

    Scope Var

---
+++ custom.go
@@ -0,0 +1,50 @@
+package main
+
+
+type weight float32
+
+var name string ="Azizul Hoq"
+
+/*type book struct{
+Title string
+Author string
+ISBN  string
+Price float32
+pages int
+}*/
+
+//var b1 book
+//b1.Title="Introuduction Golang Book"
+//b1.Author="CALEB DOXY"
+//b1.ISBN="978-1478355823"
+//b1.Price=10.56
+//b1.pages=165
+		
+//fmt.Println(b1)
+//fmt.Println(b1.Title)
+//fmt.Println(b1.Author)
+//fmt.Println(b1.ISBN)
+//fmt.Println(b1.Price)
+//fmt.Println(b1.pages)
+
+/*b1 := struct{
+Title string
+Author string
+ISBN  string
+Price float32
+pages int
+	
+			}{
+Title:"Introuduction Golang Book",
+Author:"CALEB DOXY",
+ISBN:"978-1478355823",
+Price:10.56,
+pages:60,
+			
+			}
+fmt.Println(b1)	
+fmt.Println(b1.Title)
+fmt.Println(b1.Author)
+fmt.Println(b1.ISBN)
+fmt.Println(b1.Price)
+fmt.Println(b1.pages)*/
---
+++ golang.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+
+func main(){
+
+
+var w1 weight
+w1=101.3
+
+fmt.Println(w1,name)
+
+	}			
